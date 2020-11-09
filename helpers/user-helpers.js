ce4e98164130d167bfada85e257fa5d1afed55a4
korathbasil <korathbasil@live.com>
2020-11-09 23:25:22 +0530

    working on user signup

---
+++ helpers/user-helpers.js
@@ -0,0 +1,17 @@
+const bcrypt = require("bcrypt");
+const db = require("../config/dbConfig");
+const collection = require("../config/collections");
+
+module.exports = {
+  doSignup: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      userData.password = await bcrypt.hash(userData.password, 10);
+      db.gerDb()
+        .collection(collection.USER_COLLECTION)
+        .insertOne(userData)
+        .then((data) => {
+          resolve();
+        });
+    });
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const userHelper = require("../helpers/user-helpers");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -11,5 +13,10 @@ router.get("/login", (req, res) => {
 router.get("/signup", (req, res) => {
   res.render("user/signup");
 });
-router.post("/user/signup", (req, res) => {});
+router.post("/user/signup", (req, res) => {
+  console.log(re.body);
+  userHelper.doSignup(req.body).then(() => {
+    res.send("user created");
+  });
+});
 module.exports = router;
